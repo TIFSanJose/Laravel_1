0ae7ca6587491716ac538353ccb75fe1a7406cf1
cafeConLeche <cafe@conleche.com>
2021-06-04 16:55:50 -0300

    >> Video: 2, > Rutas
    .: Se realizan los ejercicios de rutas.
    .: Crear rutas, pasar valores por las rutas, todo anduvo bien.
    .: Modifica el .gitignore del proyecto y de las carpetas internas.
    
    [url:https://www.youtube.com/playlist?list=PLZ2ovOgdI-kWWS9aq8mfUDkJRfYib-SvF]

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "video 2 del curso laravel";
+});
+
+Route::get('curso', function() {
+    return "video 2 del curso laravel";
+});
+
+// Route::get('curso/{lengauaje}', function($lenguaje) {
+//     return "bienvenido al curso de $lenguaje";
+// });
+
+// Route::get('curso/{lenguaje}/{so}', function($lenguaje, $so) {
+//     return "bienvenido al curso de $lenguaje en so $so";
+// });
+
+Route::get('curso/{lenguaje}/{so?}', function($lenguaje, $so = null) {
+    if($so){
+        return "bienvenido al curso de $lenguaje en so $so";
+    }else{
+        return "bienvenido al curso de $lenguaje";
+    }
 });
