6f4e78407cc0b822161cff0ccc8d89962b2b2513
cafeConLeche <cafe@conleche.com>
2021-06-09 13:44:24 -0300

    >> Video: 7, > Creando Migraciones
    .: Se crea migracion y se ejecuta.
    .: Se revisa BD, y funciono.
    
    [url:https://www.youtube.com/playlist?list=PLZ2ovOgdI-kWWS9aq8mfUDkJRfYib-SvF]

---
+++ database/migrations/2021_06_09_164005_cursos.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Cursos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::create('curso', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('descripcion');
+            $table->timestamps();
+
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        
+        Schema::dropIfExists('curso');
+        
+    }
+}
